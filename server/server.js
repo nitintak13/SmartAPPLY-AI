bf89a4bb49ae871f45743d4873c48bea93db5789
Nitin tak <129604612+nitintak13@users.noreply.github.com>
2025-07-30 11:12:33 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -14,8 +14,8 @@ const app = express();
 connectDB();
 await connectCloudinary();
 const corsOptions = {
-  // origin: "https://smart-apply-indol.vercel.app",
-  origin: "http://localhost:5173",
+  origin: "https://smartapply-ai.onrender.com",
+  // origin: "http://localhost:5173",
   credentials: true,
 };
 app.options("*", cors(corsOptions));
